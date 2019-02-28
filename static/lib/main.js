0268a6f8825ed0bdcac05973184e6900d532c9ae
Julian Lam <julian@nodebb.org>
2019-02-28 17:32:22 -0500

    fix: updated login button to confirm to branding guidelines
    https://developers.facebook.com/docs/facebook-login/userexperience#buttondesign
    https://en.facebookbrand.com/#brand-guidelines-assets

---
+++ static/lib/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(window).on('action:script.load', function (ev, data) {
+	data.scripts.push('sso-facebook/login');
+});
+
+define('sso-facebook/login', function () {
+	var Login = {};
+
+	Login.init = function () {
+		var replaceEl = $('.alt-logins .facebook a i');
+		var replacement = document.createElement('div');
+		var image = document.createElement('img');
+		image.src = config.relative_path + '/plugins/nodebb-plugin-sso-facebook/images/f-ogo_RGB_HEX-58.svg';
+		replaceEl.replaceWith(replacement);
+		replacement.append(image);
+		$('<span>Continue with Facebook</span>').appendTo(replacement)
+	}
+
+	return Login;
+})

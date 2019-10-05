98070bb694f2e3d49dc40dd2533e8a103c5488f6
Andrew Rodrigues <psycho.bunny@hotmail.com>
2019-10-04 20:03:06 -0400

    fix: error when ajaxifying on IE
    though, probably check? I think this doesn't work in chrome either (but doesn't blow up), just incase

--- static/lib/main.js
+++ static/lib/main.js
@@ -13,7 +13,7 @@ define('sso-facebook/login', function () {
 		var image = document.createElement('img');
 		image.src = config.relative_path + '/plugins/nodebb-plugin-sso-facebook/images/f-ogo_RGB_HEX-58.svg';
 		replaceEl.replaceWith(replacement);
-		replacement.append(image);
+		replacement.appendChild(image);
 		$('<span>Continue with Facebook</span>').appendTo(replacement);
 	}
 

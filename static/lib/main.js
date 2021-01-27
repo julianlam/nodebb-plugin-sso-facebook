a2efc6672687e1cdbd7c8c37757aab7e7c7b64ea
Julian Lam <julian@nodebb.org>
2021-01-27 15:50:19 -0500

    fix: update login button replacement code to use new hook system

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,21 +1,15 @@
 'use strict';
 
-$(window).on('action:script.load', function (ev, data) {
-	data.scripts.push('sso-facebook/login');
+require(['hooks'], (hooks) => {
+	hooks.on('static:script.init', ({ tpl_url }) => {
+		if (tpl_url === 'login') {
+			var replaceEl = $('.alt-logins .facebook a i');
+			var replacement = document.createElement('div');
+			var image = document.createElement('img');
+			image.src = config.relative_path + '/plugins/nodebb-plugin-sso-facebook/images/f-ogo_RGB_HEX-58.svg';
+			replaceEl.replaceWith(replacement);
+			replacement.appendChild(image);
+			$('<span>Continue with Facebook</span>').appendTo(replacement);
+		}
+	});
 });
-
-define('sso-facebook/login', function () {
-	var Login = {};
-
-	Login.init = function () {
-		var replaceEl = $('.alt-logins .facebook a i');
-		var replacement = document.createElement('div');
-		var image = document.createElement('img');
-		image.src = config.relative_path + '/plugins/nodebb-plugin-sso-facebook/images/f-ogo_RGB_HEX-58.svg';
-		replaceEl.replaceWith(replacement);
-		replacement.appendChild(image);
-		$('<span>Continue with Facebook</span>').appendTo(replacement);
-	}
-
-	return Login;
-})

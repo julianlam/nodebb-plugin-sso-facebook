12e739dc7a12fbc46c8505ff7bca1a5f7a94f96b
Julian Lam <julian@nodebb.org>
2021-01-28 16:09:26 -0500

    fix: use action:ajaxify.end hook instead

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require(['hooks'], (hooks) => {
-	hooks.on('static:script.init', ({ tpl_url }) => {
+	hooks.on('action:ajaxify.end', ({ tpl_url }) => {
 		if (tpl_url === 'login') {
 			var replaceEl = $('.alt-logins .facebook a i');
 			var replacement = document.createElement('div');
